cffeb397f9eca0e3e752abbb396b938605683ba0
BlodTor <blodtor@gmail.com>
2025-11-13 23:09:40 +0300

    Joy test for SGDK 2.12 GCC 13.2 and GCC 6.3

---
+++ res/resources.h
@@ -0,0 +1,14 @@
+#include <genesis.h>
+
+#ifndef _RES_RESOURCES_H_
+#define _RES_RESOURCES_H_
+
+extern const u8 cherry_sfx[20224];
+extern const u8 sonic_vgm[21760];
+extern const u8 comixzone_vgm[73728];
+extern const Image bg_sega;
+extern const SpriteDefinition rapuncel;
+extern const TileSet sega_tileset;
+extern const MapDefinition sega_map;
+
+#endif // _RES_RESOURCES_H_
